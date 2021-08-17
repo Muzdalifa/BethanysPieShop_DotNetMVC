181a164536f283a041f5fee2e23c50b83d8ad852
Muzdalifa <a.muzdalifa27@gmail.com>
2021-08-17 14:24:22 +0200

    Add project files.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,36 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            //return View(_pieRepository.AllPies);
+
+            //instead of accessing the above one by one, we can create a viewModel to return them both in the same file
+
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+
+            pieListViewModel.CurrentCategory = "Cheese cakes";
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category
+                {
+                    CategoryId = 1,
+                    CaategoryName="Fruit pies",
+                    Description = "All fruits pies"
+                },
+                new Category{
+                    CategoryId = 2,
+                    CaategoryName="Cheese cakes",
+                    Description = "Cheesy all the way.."
+                },
+                new Category{
+                    CategoryId = 3,
+                    CaategoryName="Seosonal pies",
+                    Description = "Get in the mood for for a seosonal pies"
+                }
+            };
+    }
+}
+
+
